74febd071345e79f6ceb4b5fa636ed9d89ca6480
labin <labin@10.10.1.3>
2017-05-18 22:16:04 -0300

    LER XML

--- LerXML01/src/lerxml01/LerXML.java
+++ LerXML01/src/lerxml01/LerXML.java
@@ -19,32 +19,43 @@ import org.jdom2.input.SAXBuilder;
  * @author labin
  */
 public class LerXML {
+
     public static void main(String[] args) throws JDOMException, IOException {
-         //Aqui você informa o nome do arquivo XML.  
-   File f = new File("C:\\Users\\labin\\Desktop\\clientes.xml");  
-  
-   //Criamos uma classe SAXBuilder que vai processar o XML4  
-   SAXBuilder sb = new SAXBuilder();  
-  
-   //Este documento agora possui toda a estrutura do arquivo.  
-   Document d = sb.build(f);  
-  
-   //Recuperamos o elemento root  
-   Element mural = d.getRootElement();  
-  
-   //Recuperamos os elementos filhos (children)  
-   List elements = mural.getChildren();  
-   Iterator i = elements.iterator();  
-  
-   //Iteramos com os elementos filhos, e filhos do dos filhos  
-   while (i.hasNext()) {  
-      Element element = (Element) i.next();  
-      System.out.println("Códido:"+ element.getAttributeValue("codigo"));  
-      System.out.println("Nome:"+ element.getAttributeValue("nome"));  
-      System.out.println("Uf:"+ element.getAttributeValue("uf"));  
-      System.out.println("Matricula:"+ element.getAttributeValue("matricula"));  
-      System.out.println("Corpo:"+ element.getChildText("corpo"));  
-   }  
+        //Aqui você informa o nome do arquivo XML.  
+        File f = new File("C:\\Users\\labin\\Desktop\\clientes.xml");
+
+        //Criamos uma classe SAXBuilder que vai processar o XML4  
+        SAXBuilder sb = new SAXBuilder();
+
+        //Este documento agora possui toda a estrutura do arquivo.  
+        Document d = sb.build(f);
+
+        //Recuperamos o elemento root  
+        Element cidades = d.getRootElement();
+
+        //Recuperamos os elementos filhos (children)  
+        List cidades1 = cidades.getChildren();
+        Iterator i = cidades1.iterator();
+
+        //Iteramos com os elementos filhos, e filhos do dos filhos  
+        while (i.hasNext()) {
+            Element cidade = (Element) i.next();
+            System.out.println("Códido:" + cidade.getAttributeValue("codigo"));
+            System.out.println("Nome:" + cidade.getAttributeValue("nome"));
+            System.out.println("Uf:" + cidade.getAttributeValue("uf"));
+            System.out.println("\n");
+
+            List clientes = cidade.getChildren();
+            Iterator j = clientes.iterator();
+
+            while (j.hasNext()) {
+                Element cliente = (Element) j.next();
+                System.out.println("Matrícula: " + cliente.getChildText("matricula"));
+                System.out.println("Nome: " + cliente.getChildText("nome"));
+                System.out.println("Nascimento: " + cliente.getChildText("nascimento"));
+                System.out.println("\n");
+            }
+        }
     }
-    
+
 }
